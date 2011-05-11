72117fb667a71fb23b211f580709a6216f5dfa97
Leif Wickland <leifwickland@gmail.com>
2011-05-11 12:27:28 -0600

    Solved #8 for old times sake

--- src/main/scala/euler.scala
+++ src/main/scala/euler.scala
@@ -1,5 +1,5 @@
 object euler {
-  val projects = Set[util.Project](project11, project16, project20, project13, project14, project25, project48, project336, project19, project28, project21)
+  val projects = Set[util.Project](project11, project16, project20, project13, project14, project25, project48, project336, project19, project28, project21, project8)
 
   def main(args: Array[String]) {
     if (args.length == 0) {
