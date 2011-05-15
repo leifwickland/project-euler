ada51af274994e94f33db18e54ae65da6e2f152c
Leif Wickland <lwickland@lwickland-VirtualBox.(none)>
2011-05-15 01:45:07 -0600

    Tried to make it faster by storing slightly less

--- src/main/scala/project15.scala
+++ src/main/scala/project15.scala
@@ -1,9 +1,9 @@
 import scala.collection.mutable.Stack
 
-
-// I doubt this will solve the problem in time I'm willing to wait given that it took about 5 minutes to solve for size 
+// Assuming it doesn't run out of memory, this will solve the problem in about 3 hours.  
 // 16 and level 20 is about 3 orders of magnitude larger.
 // This code was sufficient to convince me that the answer could be found in oeis.org; it's http://oeis.org/A000984.
+// It turns out, there are way, way easier ways to solve it once you know that.
 object project15 extends util.Project {
   def description = "How many routes are there through a 20x20 grid?"
 
@@ -18,44 +18,41 @@ object project15 extends util.Project {
   def getRoutes(gridSize: Int): Long = {
     val levels = gridSize * 2 + 1
     val lastLevel = levels - 1
+    val penultimateLevel = lastLevel - 1
     val midLevel = gridSize
     val maxIndices: Vector[Int] = Vector[Int]() ++ 0.to(midLevel) ++ (midLevel-1).to(0,-1)
     var routes = 0: Long
 
-    case class Route(level: Int, path: List[Int]) {
-      override def toString = "%d: %s".format(this.level, this.path.reverse)
-
+    case class Route(level: Int, index: Int) {
       def getChildren: List[Route] = if (this.level < midLevel) this.getChildrenTop else this.getChildrenBottom
       private def getChildrenTop: List[Route] = List[Route](this.birth(0), this.birth(1))
       private def getChildrenBottom: List[Route] = {
-        this.path.head match {
+        this.index match {
           case 0 => List[Route](this.birth(0))
           case index: Int if index == maxIndices(this.level) => List[Route](this.birth(-1))
           case _ => List[Route](this.birth(-1), this.birth(0)) 
         }
       }
-      private def birth(delta: Int) = new Route(this.level + 1, (this.path.head + delta) +: this.path)
+      private def birth(delta: Int) = new Route(this.level + 1, (this.index + delta))
     }
 
-    val stack = Stack[Route](new Route(1, List[Int](0))) // I know the routes are symetric, so only solve half the problem
+    val stack = Stack[Route](new Route(1, 0)) // I know the routes are symmetric, so only solve half the left half of the problem
     while (!stack.isEmpty) {
       val route = stack.pop
-      if (route.level == lastLevel) {
+      if (route.level >= penultimateLevel)
         routes += 1 
-      }
-      else {
+      else 
         stack.pushAll(route.getChildren)
-      }
     }
 
-    return 2 * routes
+    return 2 * routes // Due to the aforementioned cheating
   }
 
   override def test() {
     verify(getRoutes(1), 2)
     verify(getRoutes(2), 6)
     verify(getRoutes(3), 20)
-    verify(getRoutes(3), 70)
+    verify(getRoutes(4), 70)
   }
 }
 
